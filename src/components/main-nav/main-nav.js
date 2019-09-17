5401a8af4ae5e4024cfa2f7ab3697d2bf0dab16d
Chris Berry <chrisberry08@gmail.com>
2019-09-16 23:22:19 -0400

    - fixed hamburger

--- src/components/main-nav/main-nav.js
+++ src/components/main-nav/main-nav.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
+import "./main-nav.css"
 import { Link } from "gatsby"
 
 const Nav = styled.nav`
@@ -24,26 +25,71 @@ const Nav = styled.nav`
       }
     }
   }
+  @media screen and (max-width: 710px) {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    top: 0;
+    left: 0;
+    padding: 20px;
+    background: linear-gradient(
+        180deg,
+        #ffffff 0%,
+        rgba(255, 255, 255, 0.75) 100%
+      ),
+      linear-gradient(285.65deg, #cc00ff 0%, #00f0ff 84.25%);
+    transform: ${({ open }) => (open ? "translateY(0)" : "translateY(-100%)")};
+    transition: transform 0.25s;
+    ul {
+      display: flex;
+      width: 100%;
+      flex-direction: column;
+      justify-content: space-evenly;
+      text-align: center;
+      li {
+        font-size: 40px;
+      }
+    }
+  }
+`
+const Hamburger = styled.button`
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      position: fixed;
+      top: 0;
+      right: 0;
+      z-index: 10;
 `
 
 const MainNav = () => {
+  const [open, setOpen] = useState(false)
+  console.log(open)
   return (
-    <Nav className="font-display">
-      <ul>
-        <li>
-          <Link to="/">Posts</Link>
-        </li>
-        <li>
-          <Link to="/bookmarks">Bookmarks</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </Nav>
+    <>
+      <Hamburger className={`hamburger hamburger--squeeze ${ open ? "is-active": null }`} type="button" onClick={() => setOpen(!open)}>
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </Hamburger>
+      <Nav className="font-display" open={open} setOpen={setOpen}>
+        <ul>
+          <li>
+            <Link to="/">Posts</Link>
+          </li>
+          <li>
+            <Link to="/bookmarks">Bookmarks</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </Nav>
+    </>
   )
 }
 
